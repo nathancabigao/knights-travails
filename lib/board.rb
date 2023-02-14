2b6f07b821c7d9cc6034f57dc4434a937c514048
nathancabigao <nathan0714@hotmail.com>
2023-02-14 17:19:44 -0500

    Refactor Knight to be Node-like, complete Board
    This commit refactors Knight to be more node-like in that it tracks
    children/parents, rather than tracking a history of locations due to
    implementation issues in the last commit.
    
    As a result, Knight also has a method #path that creates an array of the
    moves from the root Knight to the current Knight.
    
    Board changes as a result of Knight refactoring, and completes the
    '#knight_moves' expectations, printing the path taken and the number of
    moves required.

--- lib/board.rb
+++ lib/board.rb
@@ -10,41 +10,32 @@ class Board
     @board = Array.new(8) { Array.new(8) }
   end
 
-  # Given start and dest positions represented as arrays (ex. [0,0], [1,2]), checks if the move is valid
   def valid_move?(start, dest)
-    # Check if start has a piece there?
-    # return false if @board[start[0]][start[1]].nil?
-    # Make sure that start is within the board
     return false unless start[0].between?(0, 7) && start[1].between?(0, 7)
-    # Make sure that end is within the board
     return false unless dest[0].between?(0, 7) && dest[1].between?(0, 7)
 
     true
   end
 
-  # Takes a starting and ending position for a knight, and prints the shortest path to the ending position
   def knight_moves(start, dest)
-    puts 'Not a valid move.' unless valid_move?(start, dest)
-    knight = Knight.new(start)
-    shortest_path_knight = find_shortest_path(knight, dest)
-    puts "You made it in #{shortest_path_knight.history.size - 1} moves! Here's your path:"
-    puts shortest_path_knight.history
+    valid = valid_move?(start, dest)
+    puts 'Not a valid move.' unless valid
+    return unless valid
+
+    shortest_path_knight = find_shortest_path(start, dest)
+    puts "You made it in #{shortest_path_knight.path.size - 1} moves! Here's your path:"
+    shortest_path_knight.path.each { |move| p move }
   end
 
-  def find_shortest_path(knight, dest)
-    return nil if knight.nil?
-
-    # queue/level-order traversal, first match is the lowest depth.
+  def find_shortest_path(start, dest)
     queue = []
-    queue << knight
-    # add current position to history array (track moves)
-    # is the current knight position the dest? if so, return as we have found the path
+    queue << Knight.new(start)
     until queue.empty?
       front = queue.shift
-      front.history << front.location
       return front if front.location == dest
 
-      front.possible_moves.each { |move| queue << Knight.new(move, front.history) unless front.history.include?(move) }
+      front.possible_moves.each { |move| front.children << Knight.new(move, front) }
+      front.children.each { |child| queue << child }
     end
   end
 end
--- lib/knight.rb
+++ lib/knight.rb
@@ -2,22 +2,32 @@
 
 # Instantiates a Knight chess piece
 class Knight
-  attr_accessor :location, :history
+  attr_accessor :location, :parent, :children
 
   MOVES = [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]].freeze
 
-  def initialize(location = [0, 0], history = [])
+  def initialize(location = [0, 0], parent = nil, children = [])
     @location = location
-    @history = history
+    @parent = parent
+    @children = children
   end
 
-  # Given the knight's location, returns an array of all the possible moves
   def possible_moves
     possible = []
     MOVES.each do |move|
-      new = @location + move
+      new = [@location[0] + move[0], @location[1] + move[1]]
       possible << new if new[0].between?(0, 7) && new[1].between?(0, 7)
     end
     possible
+  end
+
+  def path
+    current = self
+    path = []
+    until current.nil?
+      path.unshift(current.location)
+      current = current.parent
+    end
+    path
   end
 end
